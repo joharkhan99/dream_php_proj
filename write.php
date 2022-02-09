3490a4fb4adb9031b70908bedc691719ee5ca9d2
single-byte <70197335+single-byte@users.noreply.github.com>
2022-02-09 22:12:23 +0500

    added write page

---
+++ write.php
@@ -0,0 +1,58 @@
+<!-- head -->
+<?php include "sections/header.php" ?>
+<!-- head -->
+<main>
+  <?php include "sections/nav.php" ?>
+
+  <section class="write-section bg-primary py-5">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-6">
+                  <h1 class="h6 text-white">START WRITING ARTICLES FOR FREE</h1>
+                  <h2 class="mt-2">Publish, learn, and contribute on Likhdy.com</h2>
+                  <p class="text-white mt-3">
+                  Assuming  you have a story to tell, information to share, or perspective to offer, welcome home. Pursue it for free so that your composition can thrive in an organization supported by a large number of users and not by advertising.
+                  </p>
+                  <a href="signup" class="btn py-2 px-4 bg-white text-dark mt-4">Start Writing</a>
+              </div>
+              <div class="col-md-6 p-4">
+                  <img src="assets/bulb.png" class="w-100 h-100" alt="idea innovation likhdy">
+              </div>
+          </div>
+      </div>
+    </section>
+    <section class="share_info py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 p-4">
+                <img src="assets/share.png" class="w-100 h-100" alt="idea innovation likhdy">
+                </div>
+                <div class="col-md-6">
+                    <blockquote>
+                        <h3 class="mt-2"><b>"</b> Writing means sharing. It's part of the human condition to want to share things - thoughts, ideas, opinions. <b>"</b></h3>
+                        <h4 class="m-0"><i>Paulo Coelho</i></h4>
+                    </blockquote>
+                    <p class="mt-5 text-white">So what are you waiting for start writing articles about various topics and matters and share them with others. Grow your online audience and help others by sharing your knowledge.</p>
+                    <a href="signup" class="btn py-2 px-4 bg-dark text-white mt-4">Start Writing</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+  <!-- newsletter -->
+  <?php include "sections/newsletter.php" ?>
+  <!-- ./newsletter -->
+
+  <!-- footer -->
+  <?php include "sections/footer.php" ?>
+  <!-- ./footer -->
+
+</main>
+
+<!-- scripts -->
+<?php include "sections/scripts.php" ?>
+<!-- scripts -->
+
+</body>
+
+</html>
